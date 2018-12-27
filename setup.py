9f243538007fc1ccbdba1d59863c79834088ee47
samuel <samuel@meyn.fr>
2018-12-27 18:04:01 +0100

    add select field for exp and dest in SME - Enveloppe

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open("requirements.txt", "r") as rp:
 
 setuptools.setup(
     name="smeadmin",
-    version="0.0.2",
+    version="0.0.3",
     author="Samuel MEYNARD",
     author_email="samuel@meyn.fr",
     description="SME admin tools",
